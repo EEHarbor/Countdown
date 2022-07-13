3d7a21d976e36c8f3d4eb2609197b8ddf6f8fe48
Bradford Akin <bakin1637@gmail.com>
2022-07-13 09:48:58 -0400

    give author credit ;)

--- countdown/addon.setup.php
+++ countdown/addon.setup.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'author'            => 'Jack McDane, EEHARBOR',
+    'author'            => 'Jack McDade, EEHarbor',
     'author_url'        => 'https://eeharbor.com/',
     'name'              => 'Countdown',
     'description'       => 'Countdown plug-in allows you to insert a simple countdown to a specified date into your templates.',
     'version'           => '1.0.0',
-    'namespace'         => 'JackMcdaneEeharbor\Countdown',
+    'namespace'         => 'EEHarbor\Countdown',
     'settings_exist'    => false,
     // Advanced settings
 ];
